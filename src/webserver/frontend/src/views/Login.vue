400e328198cacf74126cf23db0b8f819d57d1674
Lernni <Lernni@users.noreply.github.com>
2021-05-04 10:54:53 +0200

    #74 Vue View Login

---
+++ src/webserver/frontend/src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="login-box card">
+      <div class="card-body">
+        <h5 class="card-title">Login</h5>
+        <form action="">
+          <div class="form-group">
+            <label for="userInput">Benutzername</label>
+            <input type="text" class="form-control" id="userInput">
+          </div>
+          <div class="form-group">
+            <label for="passwordInput">Passwort</label>
+            <input type="password" class="form-control" id="passwordInput">
+          </div>
+          <button type="submit" class="btn btn-primary">Anmelden</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style>
+
+</style>
