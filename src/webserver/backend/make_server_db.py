97764cd586917a8cbd51a973d56530e4395578b5
max <max.haufe@htw-dresden.de>
2021-05-10 17:40:10 +0200

    #73 python script zur erstellung der DB des Webservers hinzugefügt

---
+++ src/webserver/backend/make_server_db.py
@@ -0,0 +1,20 @@
+import sqlite3
+import os.path
+
+if __name__ == "__main__":
+    if not os.path.exists("database_server.db"):
+        first_connection = sqlite3.connect("database_server.db")
+        c = first_connection.cursor()
+        fd = open('database_server.sql', 'r')
+        sql_file = fd.read()
+        fd.close()
+        sql_commands = sql_file.split(';')
+
+        for command in sql_commands:
+            c.execute(command)
+
+        first_connection.commit()
+        first_connection.close()
+    else:
+        print("db already exists")
+        exit(1)
