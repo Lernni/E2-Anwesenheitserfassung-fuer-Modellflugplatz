fdb2b830d5e52a6006f06eb251f58eec930cbecd
Dirk Zimmermann <s80519@htw-dresden.de>
2021-05-15 17:57:56 +0200

    Grundgerüst Terminal

---
+++ src/Terminal/databaseAccess.py
@@ -0,0 +1,25 @@
+import sqlite3
+import importlib
+
+serverConnection = importlib.import_module("serverConnection.py")
+
+def createSession():
+    return
+
+def endSession(SessionID):
+    return
+
+def getSession(SessioID):
+    return
+
+def setFlugleiter(RFID_Code):
+    return
+
+def getPilot(RFID_Code):
+    return
+
+def syncSession(SessionID):
+    return
+
+def syncPilot():
+    return
---
+++ src/Terminal/userInterface.py
@@ -0,0 +1,6 @@
+import importlib
+
+databaseAccess = importlib.import_module("databaseAccess.py")
+
+if __name__ == "__main__":
+    pass
