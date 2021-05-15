b2c201abf2a773a309a155b7d38764c6dab04f60
max <max.haufe@htw-dresden.de>
2021-05-15 18:04:25 +0200

    #85 GET /pilot-list

---
+++ src/webserver/backend/pilot_list.py
@@ -0,0 +1,23 @@
+from flask_restx import Resource
+
+from globals import get_connection
+
+
+# AddSession.vue
+# GET Request: /pilot-list -> Backend liefert aktive Piloten: {pilot_id, pilot_name}
+class PilotList(Resource):
+    def get(self):
+        '''get id, and name of every pilot'''
+        connection = get_connection("database_server.db")
+        cursor = connection.cursor()
+        return_dict = {
+            'pilots': []
+        }
+        for row in cursor.execute('SELECT ROWID, Vorname, Nachname FROM Pilot'):
+            pilot = {
+                'pilot_id': row[0],
+                'pilot_name': row[1] + " " + row[2]
+            }
+            return_dict['pilots'].append(pilot)
+        connection.close()
+        return return_dict
