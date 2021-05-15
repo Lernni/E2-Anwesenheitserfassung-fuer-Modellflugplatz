7a0e22c925dd70376fb864d75ad63806a39dadca
max <max.haufe@htw-dresden.de>
2021-05-15 17:54:32 +0200

    #73 PK(ID) entfernt und durch ROWID ersetzt

--- src/Terminal/database_terminal.sql
+++ src/Terminal/database_terminal.sql
@@ -6,32 +6,30 @@ CREATE TABLE IF NOT EXISTS RFID_Ausweis
     RFID_Code integer PRIMARY KEY
 );
 
+-- keine PilotID, stattdessen: ROWID
 CREATE TABLE IF NOT EXISTS Pilot
 (
-    Pilot_ID       integer,
     RFID_Code      integer,
     Nachname       varchar(20) NOT NULL,
     Vorname        varchar(20) NOT NULL,
     Eintrittsdatum date        NOT NULL,
     Ist_Aktiv      bool,
 
-    PRIMARY KEY (Pilot_ID),
--- ok so?!
+-- TODO
     FOREIGN KEY (RFID_Code) references RFID_Ausweis (RFID_Code) ON DELETE SET NULL,
     UNIQUE (RFID_Code)
 );
 
+-- keine SessionID, stattdessen: ROWID
 CREATE TABLE IF NOT EXISTS Flugsession
 (
-    SessionID      integer,
     -- Not NULL sinnvoll?! TODO
     PilotID        integer,
     Startzeit      date NOT NULL,
     Endzeit        date,
     -- Not NULL sinnvoll?!
-    Ist_Flugleiter bool NOT NULL,
+    Ist_Flugleiter bool NOT NULL DEFAULT FALSE,
 
-    PRIMARY KEY (SessionID),
 --     TODO
-    FOREIGN KEY (PilotID) REFERENCES Pilot (Pilot_ID) ON DELETE SET NULL
+    FOREIGN KEY (PilotID) REFERENCES Pilot (ROWID) ON DELETE SET NULL
 );
