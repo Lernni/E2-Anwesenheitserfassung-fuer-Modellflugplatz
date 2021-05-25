1dc6f75d464e96d9ce6d6234c46b03d8e13db431
max <max.haufe@htw-dresden.de>
2021-05-25 15:42:00 +0200

    #84, #78 - Pilot am Terminal an- und abmelden

---
+++ src/Terminal/terminal.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import sys
+
+from databaseAccess import create_session, end_session, get_active_sessions
+
+sys.path.append('/home/pi/MFRC522-python')
+from mfrc522 import SimpleMFRC522
+
+
+def eval_rfid(rfid):
+    ret = get_active_sessions(rfid)
+
+    if ret == []:
+        # keine aktive session mit rfid code vorhanden
+        # -> neue session
+        try:
+            create_session(rfid)
+        except ValueError as e:
+            # todo: logging?
+            print('ERROR: ' + str(e))
+    else:
+        # session beenden
+        end_session(ret[0]['session_id'])
+
+
+if __name__ == "__main__":
+    reader = SimpleMFRC522()
+    print("Now place tag next to the scanner to read")
+
+    try:
+        rfid_c, text = reader.read()
+        print(rfid_c)
+        eval_rfid(rfid_c)
+    finally:
+        GPIO.cleanup()
