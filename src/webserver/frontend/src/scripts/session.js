b484781bfa6e0f4a4a2262ba9ec517f1a1507c48
Lernni <Lernni@users.noreply.github.com>
2021-06-01 20:29:28 +0200

    #91 View: Flugsession bearbeiten

---
+++ src/webserver/frontend/src/scripts/session.js
@@ -0,0 +1,14 @@
+export const formSession = {
+  data() {
+    return {
+      form: {
+        startTime: "",
+        endTime: "",
+        date: null,
+        sessionLeader: false,
+        guestName: null,
+        guestText: null,
+      }
+    }
+  }
+}
