a5aa19587e827ce77e261d0d21e2e7f43184bb51
max <max.haufe@htw-dresden.de>
2021-06-04 15:32:46 +0200

    Ist_aktiv entfernt

--- src/webserver/backend/pilots.py
+++ src/webserver/backend/pilots.py
@@ -1,29 +1,26 @@
-import sqlite3
 from flask_restx import Resource, inputs, fields
-
 from globals import api, get_connection
-
-# EditPilot.vue
 
 pilots_parser = api.parser()
 pilots_parser.add_argument('id', type=int)
 pilots_parser.add_argument('is_active', type=inputs.boolean)
 
 pilot_post_model = api.model('pilot_post_model', {
-    'first_name': fields.String(description='Vorname des Piloten'),
-    'last_name': fields.String(description='Nachname des Piloten'),
-    'rfid': fields.Integer(description='RFID Tag des Piloten')
+    'pilot_name': fields.String(description='Vorname des Piloten', required=True),
+    'pilot_surname': fields.String(description='Nachname des Piloten', required=True),
+    'rfid': fields.String(description='RFID Tag des Piloten', required=True),
+    'pilot_username': fields.String(description='Benutzername des Piloten', required=True),
+    'is_admin': fields.Boolean(required=True)
 })
 
 pilot_put_model = api.model('pilot_put_model', {
     'pilot_id': fields.Integer(description='PilotID', required=True),
     'pilot_name': fields.String(description='Vorname des Piloten'),
     'pilot_surname': fields.String(description='Nachname des Piloten'),
-    'rfid': fields.Integer(description='RFID Tag des Piloten'),
+    'rfid': fields.String(description='RFID Tag des Piloten'),
     'pilot_username': fields.String(description='Benutzername des Piloten'),
     'reset_password': fields.Boolean(description='Setzt das Passwort des Piloten auf NULL, wenn true'),
-    'is_admin': fields.Boolean,
-    'is_active': fields.Boolean,
+    'is_admin': fields.Boolean
 })
 
 
@@ -43,19 +40,34 @@ class Pilots(Resource):
 
         # /pilots?is_active=true
         if p_id is None:
-            select_stmt = cursor.execute(
-                'SELECT PilotID, Vorname, Nachname, Eintrittsdatum, RFID_Code, Ist_Aktiv, Nutzername, Ist_Admin FROM Pilot WHERE Ist_Aktiv IS ?',
-                [is_active])
+            if is_active:
+                select_stmt = cursor.execute(
+                    'SELECT PilotID, Vorname, Nachname, Eintrittsdatum, RFID_Code, Nutzername, Ist_Admin '
+                    'FROM Pilot WHERE RFID_Code IS NOT NULL '
+                )
+            else:
+                select_stmt = cursor.execute(
+                    'SELECT PilotID, Vorname, Nachname, Eintrittsdatum, RFID_Code, Nutzername, Ist_Admin '
+                    'FROM Pilot WHERE RFID_Code IS NULL '
+                )
         # /pilots?id=1
         elif is_active is None:
             select_stmt = cursor.execute(
-                'SELECT PilotID, Vorname, Nachname, Eintrittsdatum, RFID_Code, Ist_Aktiv, Nutzername, Ist_Admin FROM Pilot WHERE PilotID = ?',
-                [p_id])
+                'SELECT PilotID, Vorname, Nachname, Eintrittsdatum, RFID_Code, Nutzername, Ist_Admin '
+                'FROM Pilot WHERE PilotID = ?', [p_id]
+            )
         # /pilots?id=1&is_active=true
         else:
-            select_stmt = cursor.execute(
-                'SELECT PilotID, Vorname, Nachname, Eintrittsdatum, RFID_Code, Ist_Aktiv, Nutzername, Ist_Admin FROM Pilot WHERE PilotID = ? AND '
-                'Ist_Aktiv IS ?', [p_id, is_active])
+            if is_active:
+                select_stmt = cursor.execute(
+                    'SELECT PilotID, Vorname, Nachname, Eintrittsdatum, RFID_Code, Nutzername, Ist_Admin '
+                    'FROM Pilot WHERE PilotID = ? AND RFID_Code IS NOT NULL', [p_id]
+                )
+            else:
+                select_stmt = cursor.execute(
+                    'SELECT PilotID, Vorname, Nachname, Eintrittsdatum, RFID_Code, Nutzername, Ist_Admin '
+                    'FROM Pilot WHERE PilotID = ? AND RFID_Code IS NULL', [p_id]
+                )
 
         for row in select_stmt:
             pilot = {
@@ -64,9 +76,8 @@ class Pilots(Resource):
                 'pilot_surname': row[2],
                 'entry_date': row[3],
                 'rfid': 'null' if row[4] is None else hex(row[4]),
-                'is_active': bool(row[5]),
-                'pilot_username': row[6],
-                'is_admin': bool(row[7])
+                'pilot_username': row[5],
+                'is_admin': bool(row[6])
             }
             return_dict['pilots'].append(pilot)
         connection.close()
@@ -80,15 +91,16 @@ class Pilots(Resource):
         payload = api.payload
         first_name = payload['pilot_name']
         last_name = payload['pilot_surname']
-        rfid = int(payload['rfid_code'], 16)
+        rfid = int(payload['rfid'], 16)
         username = payload['pilot_username']
         is_admin = bool(payload['is_admin'])
 
         connection = get_connection("database_server.db")
         cursor = connection.cursor()
         cursor.execute(
-            'INSERT INTO Pilot(RFID_Code, Nachname, Vorname, Eintrittsdatum, Ist_Aktiv, Nutzername, Passwort, Ist_Admin) VALUES(?, ?, ?, date(), true, ?, NULL, ?)',
-            [rfid, last_name, first_name, username, is_admin])
+            'INSERT INTO Pilot(RFID_Code, Nachname, Vorname, Eintrittsdatum, Nutzername, Passwort, Ist_Admin) '
+            'VALUES(?, ?, ?, date(), ?, NULL, ?)', [rfid, last_name, first_name, username, is_admin]
+        )
         connection.commit()
         connection.close()
         return {}
@@ -102,7 +114,6 @@ class Pilots(Resource):
         connection = get_connection("database_server.db")
         cursor = connection.cursor()
 
-        # da sqlite keine funktion zum updaten einer variablen anzahl von spalten anbietet, wird es ab hier hässlich
         if 'pilot_name' in payload.keys():
             new_first_name = payload['pilot_name']
             cursor.execute('UPDATE Pilot SET Vorname = ? WHERE PilotID = ?', [new_first_name, p_id])
@@ -128,12 +139,8 @@ class Pilots(Resource):
 
         if 'reset_password' in payload.keys():
             new_password = payload['reset_password']
-            if (new_password):
+            if new_password:
                 cursor.execute('UPDATE Pilot SET Passwort = NULL WHERE PilotID = ?', [p_id])
-
-        if 'is_active' in payload.keys():
-            new_active = payload['is_active']
-            cursor.execute('UPDATE Pilot SET Ist_Aktiv = ? WHERE PilotID = ?', [new_active, p_id])
 
         connection.commit()
         connection.close()
