98cf2424bd0a0bc181013aa2cd0fb17d5bd90579
Lernni <Lernni@users.noreply.github.com>
2021-06-17 23:52:03 +0200

    #98 Update Deployment Scripts

--- src/webserver/build.sh
+++ src/webserver/build.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-cd ~
+dir="/home/ubuntu"
+
+cd $dir
 git clone --depth 3 --filter=blob:none --sparse https://github.com/Lernni/E2-Anwesenheitserfassung-fuer-Modellflugplatz
-cd ~/E2-Anwesenheitserfassung-fuer-Modellflugplatz/
+cd $dir/E2-Anwesenheitserfassung-fuer-Modellflugplatz/
 git sparse-checkout set src/webserver/frontend/dist
 cd src/webserver/frontend/dist
 sudo rm -rf /var/www/vue/*.*
-cp -r * /var/www/vue
+cp -r -v * /var/www/vue
 
-cd ~/E2-Anwesenheitserfassung-fuer-Modellflugplatz/
+cd $dir/E2-Anwesenheitserfassung-fuer-Modellflugplatz/
 git sparse-checkout set src/webserver/backend
 cd src/webserver/backend
-cp -r * ~/backend
+cp -r -v * $dir/backend
 
-cd ~/backend
+cd $dir/backend
 source .venv/bin/activate
 pip install -r requirements.txt
 deactivate
 
-sudo rm -rf ~/E2-Anwesenheitserfassung-fuer-Modellflugplatz/
+sudo rm -rf $dir/E2-Anwesenheitserfassung-fuer-Modellflugplatz/
 sudo systemctl restart apache2
 sudo killall python
-sudo ~/backend/.venv/bin/python -m flask run &
+sudo $dir/backend/.venv/bin/python -m flask run &
--- src/webserver/install.sh
+++ src/webserver/install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+dir="/home/ubuntu"
+
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt update
 sudo apt install -y apache2 python python3-pip git
 sudo pip3 install virtualenv
 
-cd ~
+cd $dir
 git clone --depth 3 --filter=blob:none --sparse https://github.com/Lernni/E2-Anwesenheitserfassung-fuer-Modellflugplatz
-cd ~/E2-Anwesenheitserfassung-fuer-Modellflugplatz/
+cd $dir/E2-Anwesenheitserfassung-fuer-Modellflugplatz/
 git sparse-checkout set src/webserver/vue.conf
 cd src/webserver
 sudo cp ./vue.conf /etc/apache2/sites-available/
@@ -16,32 +18,32 @@ sudo a2ensite vue.conf
 sudo a2dissite 000-default.conf
 
 sudo mkdir /var/www/vue
-sudo chown -R $USER:$USER /var/www/vue
+sudo chown -R $dir:$dir /var/www/vue
 sudo chmod -R 755 /var/www/vue
 
-cd ~/E2-Anwesenheitserfassung-fuer-Modellflugplatz/
+cd $dir/E2-Anwesenheitserfassung-fuer-Modellflugplatz/
 git sparse-checkout set src/webserver/frontend/dist
 cd src/webserver/frontend/dist
 sudo rm -rf /var/www/vue/*.*
-cp -r * /var/www/vue
+cp -r -v * /var/www/vue
 
-sudo mkdir ~/backend
-sudo chown -R $USER:$USER ~/backend
-sudo chmod -R 755 ~/backend
+sudo mkdir $dir/backend
+sudo chown -R $dir:$dir $dir/backend
+sudo chmod -R 755 $dir/backend
 
-cd ~/E2-Anwesenheitserfassung-fuer-Modellflugplatz/
+cd $dir/E2-Anwesenheitserfassung-fuer-Modellflugplatz/
 git sparse-checkout set src/webserver/backend
 cd src/webserver/backend
-cp -r * ~/backend
+cp -r -v *.* $dir/backend
 
-cd ~/backend
+cd $dir/backend
 sudo virtualenv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 python make_server_db.py
 deactivate
 
-sudo rm -rf ~/E2-Anwesenheitserfassung-fuer-Modellflugplatz/
+sudo rm -rf $dir/E2-Anwesenheitserfassung-fuer-Modellflugplatz/
 sudo systemctl restart apache2
 sudo killall python
-sudo ~/backend/.venv/bin/python -m flask run &
+sudo $dir/backend/.venv/bin/python -m flask run &
