c21e7451c2a8bc0e10d7c9d1543e2c297889dc49
Lernni <Lernni@users.noreply.github.com>
2021-06-21 10:13:47 +0200

    #85 GET /settings

--- src/webserver/backend/settings.py
+++ src/webserver/backend/settings.py
@@ -2,7 +2,7 @@ import json
 
 from flask_restx import Resource
 
-from globals import api, get_connection, is_admin
+from globals import api, get_connection, is_admin, auth_parser
 
 settings_post_model = api.model('settings_post_model', {})
 
@@ -11,7 +11,7 @@ settings_post_model = api.model('settings_post_model', {})
 class Settings(Resource):
     @api.expect(settings_post_model)
     def post(self):
-        '''post settings.json to server'''
+        '''post settings to server'''
 
         connection = get_connection("database_server.db")
         cursor = connection.cursor()
@@ -24,3 +24,17 @@ class Settings(Resource):
         file.write(json.dumps(api.payload))
         file.close()
         return {}
+
+    @api.expect(auth_parser)
+    def get(self):
+        '''get settings'''
+        connection = get_connection("database_server.db")
+        cursor = connection.cursor()
+
+        if not is_admin(cursor):
+            connection.close()
+            return {}, 401
+
+        with open('settings.json') as settings_file:
+            return json.load(settings_file)
+        
