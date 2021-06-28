a6ea0ea6352a28b962f85de6b368e8cc662d5e6b
Lernni <Lernni@users.noreply.github.com>
2021-06-28 11:41:33 +0200

    Organisation Belegabgabe SE II

--- src/webserver/frontend/src/views/ImprintPrivacy.vue
+++ src/webserver/frontend/src/views/ImprintPrivacy.vue
@@ -1,3 +1,10 @@
+<!--
+  *** ImprintPrivacy.vue ***
+  - Impressum & Datenschutz
+  - Autor: Lenny Reitz, Alexander Heiß
+  - Mail: lenny.reitz@htw-dresden.de, alexander.heiss@htw-dresden.de
+-->
+
 <template>
   <div class="imprint-privacy">
     <h1>Impressum</h1>
