e98918f9774737b4ee19797714758c04761037fb
Lernni <Lernni@users.noreply.github.com>
2021-06-30 00:01:45 +0200

    #91 Impressum & Datenschutz eingefügt

--- src/webserver/frontend/src/views/ImprintPrivacy.vue
+++ src/webserver/frontend/src/views/ImprintPrivacy.vue
@@ -8,9 +8,75 @@
 <template>
   <div class="imprint-privacy">
     <h1>Impressum</h1>
+    Anschrift:<br>
+    MODELLFLUGCLUB ROSSENDORF e.V.<br>
+    Bautzner Landstraße 26<br>
+    01454 Radeberg - OT Großerkmannsdorf<br><br>
 
-    <h1>Datenschutz</h1>
+    <h3>Kontakt</h3>
+    Kai Wenzel: Tel. 0173 3571327<br><br>
+
+    email: info@mfc-rossendorf.de  <br><br>
+
+    Der Modellflugclub Rossendorf e.V. ist als gemeinnütziger Verein beim Amtsgericht Dresden mit der Registriernummer VR 1811 angemeldet und ist anerkannter Träger der freien Jugendhilfe.<br><br>
+
+    <h3>Vorstand</h3>
+    Vorsitzender:	Thomas Brenner
+    <br>Stellv. Vorsitzender:	Prof. Dr. Norbert Brückner
+    <br>Schatzmeister:	Ulrich Friedemann
     
+
+    
+    <br><br>
+    <h3>Rechtliche Hinweise</h3>
+    Rechtliche Hinweise zur Haftung
+
+    <br><br>Trotz sorgfältiger Bemühungen können wir keine Gewähr für Richtigkeit, Vollständigkeit und Aktualität unserer Webseiten geben. Weiter übernehmen wir keine Haftung für die Inhalte externer Links.
+
+    Der Inhalt des Internetauftritts enthält keine Angebote des Rechts und mit dem Inhalt des Internetauftritts werden keine rechtlichen Verpflichtungen eingegangen. Im Sinne des Rechts enthält der Internetauftritt keine Empfehlungen. 
+
+    Für den Inhalt dieser Seiten sind ausschließlich deren Betreiber verantwortlich. Der mfcR behält es sich vor, Teile des Internetangebotes oder das gesamte Angebot zu ändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen.
+
+    Mit Urteil vom 12. September 1999 hat das Landgericht Hamburg entschieden, dass man durch Setzen eines Links die Inhalte der gelinkten Seite ggf. mit zu verantworten hat. Dies kann, so das LG, nur dadurch verhindert werden, indem man sich ausdrücklich von diesen Inhalten distanziert. Wir haben auf unseren Seiten Links zu anderen Seiten im Internet gelegt. Für alle diese Links gilt: Dass wir keinerlei Einfluss auf die Gestaltung und die Inhalte der gelinkten Seiten haben. Deshalb distanzieren wir uns hiermit ausdrücklich von allen Inhalten aller gelinkten Seiten auf unserer Internetpräsenz und machen uns diese Inhalte NICHT zu Eigen. Diese Erklärung gilt für alle auf unserer Internetpräsenz angezeigten Links. 
+
+    <hr>
+    <h1>Datenschutzerklärung</h1>
+    <p>Verantwortlicher im Sinne der Datenschutzgesetze, insbesondere der EU-Datenschutzgrundverordnung (DSGVO), ist:</p>
+    <p>MODELLFLUGCLUB ROSSENDORF e.V.
+    <br>Bautzner Landstraße 26
+    <br>01454 Radeberg - OT Großerkmannsdorf
+    <br>Kai Wenzel: Tel. 0173 3571327
+    <br>email: info@mfc-rossendorf.de</p>
+    <h3>Ihre Betroffenenrechte</h3>
+    <p>Unter den angegebenen Kontaktdaten unseres Datenschutzbeauftragten können Sie jederzeit folgende Rechte ausüben:</p>
+    <ul>
+    <li>Auskunft über Ihre bei uns gespeicherten Daten und deren Verarbeitung (Art. 15 DSGVO),</li>
+    <li>Berichtigung unrichtiger personenbezogener Daten (Art. 16 DSGVO),</li>
+    <li>Löschung Ihrer bei uns gespeicherten Daten (Art. 17 DSGVO),</li>
+    <li>Einschränkung der Datenverarbeitung, sofern wir Ihre Daten aufgrund gesetzlicher Pflichten noch nicht löschen dürfen (Art. 18 DSGVO),</li>
+    <li>Widerspruch gegen die Verarbeitung Ihrer Daten bei uns (Art. 21 DSGVO) und</li>
+    <li>Datenübertragbarkeit, sofern Sie in die Datenverarbeitung eingewilligt haben oder einen Vertrag mit uns abgeschlossen haben (Art. 20 DSGVO).</li>
+    </ul>
+    <p>Sofern Sie uns eine Einwilligung erteilt haben, können Sie diese jederzeit mit Wirkung für die Zukunft widerrufen.</p>
+    <p>Sie können sich jederzeit mit einer Beschwerde an eine Aufsichtsbehörde wenden, z. B. an die zuständige Aufsichtsbehörde des Bundeslands Ihres Wohnsitzes oder an die für uns als verantwortliche Stelle zuständige Behörde.</p>
+    <p>Eine Liste der Aufsichtsbehörden (für den nichtöffentlichen Bereich) mit Anschrift finden Sie unter: <a href="https://www.bfdi.bund.de/DE/Service/Anschriften/Laender/Laender-node.html" target="_blank" rel="noopener nofollow">https://www.bfdi.bund.de/DE/Service/Anschriften/Laender/Laender-node.html</a>.</p>
+    <p></p><hr>
+    <h3>Information über Ihr Widerspruchsrecht nach Art. 21 DSGVO</h3>
+    <h4>Einzelfallbezogenes Widerspruchsrecht</h4>
+    <p>Sie haben das Recht, aus Gründen, die sich aus Ihrer besonderen Situation ergeben, jederzeit gegen die Verarbeitung Sie betreffender personenbezogener Daten, die aufgrund Art. 6 Abs. 1 lit. f DSGVO (Datenverarbeitung auf der Grundlage einer Interessenabwägung) erfolgt, Widerspruch einzulegen; dies gilt auch für ein auf diese Bestimmung gestütztes Profiling im Sinne von Art. 4 Nr. 4 DSGVO.</p>
+    <p>Legen Sie Widerspruch ein, werden wir Ihre personenbezogenen Daten nicht mehr verarbeiten, es sei denn, wir können zwingende schutzwürdige Gründe für die Verarbeitung nachweisen, die Ihre Interessen, Rechte und Freiheiten überwiegen, oder die Verarbeitung dient der Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen.</p>
+    <h4>Empfänger eines Widerspruchs</h4>
+    <p>MODELLFLUGCLUB ROSSENDORF e.V.
+    <br>Bautzner Landstraße 26
+    <br>01454 Radeberg - OT Großerkmannsdorf
+    <br>Kai Wenzel: Tel. 0173 3571327
+    <br>email: info@mfc-rossendorf.de</p>
+    <hr>
+    <h3>Änderung unserer Datenschutzbestimmungen</h3>
+    <p>Wir behalten uns vor, diese Datenschutzerklärung anzupassen, damit sie stets den aktuellen rechtlichen Anforderungen entspricht oder um Änderungen unserer Leistungen in der Datenschutzerklärung umzusetzen, z.B. bei der Einführung neuer Services. Für Ihren erneuten Besuch gilt dann die neue Datenschutzerklärung.</p>
+    <h3>Fragen an den Datenschutzbeauftragten</h3>
+    <p>Wenn Sie Fragen zum Datenschutz haben, schreiben Sie uns bitte eine E-Mail oder wenden Sie sich direkt an die für den Datenschutz verantwortliche Person in unserer Organisation:</p>
+    <p></p><p><em>Die Datenschutzerklärung wurde mithilfe der activeMind AG erstellt, den Experten für <a href="https://www.activemind.de/datenschutz/datenschutzbeauftragter/" target="_blank" rel="noopener">externe Datenschutzbeauftragte</a> (Version #2020-09-30).</em></p>
   </div>
 </template>
 
